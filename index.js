f34f964f28cd6f1e639f0748799886f091c1712b
ThanhDinh20198689 <150313284+ThanhDinh20198689@users.noreply.github.com>
2024-08-30 16:20:44 +0700

    5.2:lấy ra chi tiết công việc

--- index.js
+++ index.js
@@ -9,14 +9,26 @@ database.connect();
 const app = express(); // Tạo một ứng dụng express
 const port = process.env.PORT; // Đặt cổng để server lắng nghe
 
-// Định nghĩa một endpoint trả về "Hello World"
+
 app.get('/tasks', async (req, res) => {
   const tasks =await Task.find({
     deleted: false
   })
 
   console.log(tasks);
-  res.send('Danh sach cong viec');
+  // res.send('Danh sach cong viec');
+  res.json(tasks);
+});
+
+app.get('/tasks/detail/:id', async (req, res) => {
+  const id= req.params.id;
+  const tasks =await Task.find({
+    _id:id,
+    deleted: false
+  })
+
+ 
+  res.json(tasks);
 });
 
 // Bắt đầu server và lắng nghe trên cổng đã định nghĩa
